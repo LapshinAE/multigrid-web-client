2b050717de5d1c6c3f499954df3f6a4477ba76c0
lapshin <Anton_Evgenich@mail.ru>
2014-03-01 14:44:01 +0400

    Init project

---
+++ multigrid_web_client/multigrid_web_client/urls.py
@@ -0,0 +1,27 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+					# User accounts
+					url(r'^$',          'job_management.views.get_main'),
+					url(r'^login/$',    'job_management.views.login'),
+					url(r'^logout/$',   'job_management.views.logout'),
+					url(r'^register/$', 'job_management.views.register'),
+					url(r'^profile/$', 'job_management.views.profile'),
+
+					# Projects
+					url(r'^jobs/$', 'job_management.views.jobs_list'),
+					url(r'^job/(?P<job_id>\d+)/$', 'job_management.views.get_job'),
+					url(r'^create_job/$', 'job_management.views.create_job'),
+					url(r'^create_loadcase/$', 'job_management.views.create_loadcase'),
+					url(r'^create_model/$', 'job_management.views.create_model'),
+					url(r'^search_job/$', 'job_management.views.search_job'),
+					url(r'^delete_job/(?P<job_id>\d+)/$', 'job_management.views.delete_job'),
+					url(r'^edit_job/(?P<job_id>\d+)/$', 'job_management.views.edit_job'),
+					url(r'^calc_job/(?P<job_id>\d+)/$', 'job_management.views.calc_job'),
+
+					# Admin
+					url(r'^admin/', include(admin.site.urls)),
+					)
